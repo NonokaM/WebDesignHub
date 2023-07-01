7d6a7db16fedd250b4865256c314904ac3063263
nonokaM <c1201071@gmail.com>
2023-07-01 13:08:27 +0900

    fix: vercel build error

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+// import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import Post from '@/components/Post'
 import styles from '../styles/home.module.css'
