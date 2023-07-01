e378de2664d64245e7438955b5c460ed4269664c
nonokaM <c1201071@gmail.com>
2023-07-01 21:46:19 +0900

    feat: データの取り出し・表示

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,8 +1,12 @@
-export default function Post({ url, comment, screenshotName }) {
+export default function Post({ url, screenshotName, comment, userId }) {
     return (
-        <>
-        <h1>{ url }</h1>
-        <p>{ comment }</p>
-        </>
-    )
+        <div className="postContainer">
+            <div className="postHeader">
+                <h1>{url}</h1>
+                <h1>{screenshotName}</h1>
+                <h1>{comment}</h1>
+                <h1>{userId}</h1>
+            </div>
+        </div>
+    );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,39 @@
 // import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import Post from '@/components/Post'
+import React, { useEffect, useState } from 'react'
+import { collection, doc, getDocs } from "firebase/firestore"
+import { auth, db } from '../lib/firebase'
 import styles from '../styles/gallery.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id})));
+    }
+    getPosts();
+  }, []);
+
   return (
     <>
-    <p className={styles.home}>home</p>
-    <Post title='test1' URL='testURL' comment='comme' />
-    <Post title='test2' URL='testURL2' comment='comme2' />
+      <p className={styles.home}>home</p>
+
+      {postList.map((post) => {
+        return post.screenshotName ? (
+          <Post
+            key={post.id}
+            url={post.url}
+            screenshotName={post.screenshotName}
+            comment={post.comment}
+            userId={post.userId}
+          />
+        ) : null;
+      })}
     </>
   )
 }
